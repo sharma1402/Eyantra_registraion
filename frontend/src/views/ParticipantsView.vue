e64044da3a5daf19f97012d22ef4291daaa1a375
sharma140345-sudo <sharma140345@gmail.com>
2026-07-25 23:27:10 +0530

    feat(ParticipantView): add participant View page

---
+++ frontend/src/views/ParticipantsView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+import { api } from '@/services/api';
+import ParticipantFilter from '@/components/ParticipantFilter.vue';
+import ParticipantsTable from '@/components/ParticipantsTable.vue';
+
+const countries = ref([]);
+const colleges = ref([]);
+const registrations = ref([]);
+const filterCountry = ref('');
+const loading = ref(false);
+
+async function loadData() {
+    loading.value = true;
+    const params = {};
+    if(filterCountry.value) params.country = filterCountry.value;
+    if(filterCollege.value) params.college = filterCollege.value;
+    registrations.value = await api.getRegistration(params)
+    loading.value = false;
+}
+
+onMounted(async () => {
+    countries.value = await api.getCountries();
+    colleges.value = await api.getColleges();
+    await loadData();
+});
+
+watch([filterCountry, filterCountry])
+</script>
+
+<template>
+  <div class="card">
+    <h2>Registered Participants</h2>
+    <ParticipantFilters :countries="countries" :colleges="colleges" v-model:country="filterCountry" v-model:college="filterCollege" />
+    <ParticipantsTable :registrations="registrations" :loading="loading" />
+  </div>
+</template>
